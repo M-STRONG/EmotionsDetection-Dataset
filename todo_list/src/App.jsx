cfb3a0de581a5d20e1673129b963c01071ab4b49
M-STRONG <mode.kik123@gmail.com>
2023-10-20 17:07:44 +0300

    Initial commit

---
+++ todo_list/src/App.jsx
@@ -0,0 +1,14 @@
+import React ,{ useState } from 'react'
+import TodoList from "./TodoList"
+import './App.css'
+
+function App() {
+ 
+  return (
+ <div>
+<TodoList></TodoList>
+ </div>
+  )
+}
+
+export default App
